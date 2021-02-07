3d8bee837e03f2ab7cda9312ad005cf280df5294
Ariel Benichou <videojr3000@gmail.com>
2021-02-07 21:01:15 +0200

    big changes: now there only one workbook to treat, no more prices workbook. added a new sheet with all the sum by clients

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # SpreadSheeter
 
-This is a script to help my father be lazy in his job.
+This is a script to help my father in his job.
 
-The program load a table with products and thier ids, each row is a client, each row is a different type of products.
+The program mulitpy each column in each sheet by the number in the first cell of the column, if the cell is empty we skip it.
 
-The program loads a second execl table with ids and prices.
+We add a sum cell at the end of each row.
 
-And then the program SPREADSHEET them - for each product the program multuply the quantity (in the first table) by the price (in the second table).
-
-At the End the program add in the last row a SUM of how much money the client must pay.
+We take all the sum cell and make a sum table in a seperate sheet.
 
 ## Install
 
@@ -23,48 +21,14 @@ If you want you can compile the program with `pyinstaller`:
 
 ## Useage
 
-The program open up with a simple UI: 2 'Browse' button and one 'Finish' button.
+The program open up with a simple UI: a 'Browse' button and one 'Finish' button.
 What to do:
 
-1. load the first table with the first button.
-2. load the seconds table with the second button.
-3. click finish!
-4. in the same folder of the first table you have an "\_out.xlsx" file.
-5. DONE!
-
-### The first table - Prices
-
-The table MUST be structered like this:
-| ID | 1111 | 3323 | 9589 |
-|--- |--- |--- |--- |
-| NAME | Cumcumbers | Red Meat | Helicopters |
-| PRICE | 3.2 | 50.3 | 50023 |
-
-AKA:
-
-1. FIRST COLUMN is the headers
-2. FIRST ROW is the ids
-3. THIRD ROW is the Prices
-
-### The seconds table - Clients
-
-The table MUST be structered like this:
-| ID | 1111 | 9589 | 3323 |
-| ----- | ----------- | ----- | --- |
-| Product Name | Cumcumbers | Helicopers | Red Meat |
-| Yossi | 1.2 | 3 | 5.2 |
-| Dr. Shipship | 0.3 | 0 | 0 |
-| Rebecca Greenred| 0 | 32 | 1.4 |
-
-AKA:
-
-1. FIRST ROW is the ids - exclude the first column
-2. SECOND ROW is the product name - for the convinience of the user, the program don't give !@#$ about it.
-3. All the other rows from the THIRD is a client with the first cell being the name of the client, under each product you put in the quantity each client wants to buy.
-4. If you have a blank cell the program fill in 0
+1. load the table with the first button.
+2. click finish!
+3. in the same folder of the first table you have an "\_out.xlsx" file.
+4. DONE!
 
 ### Useful to know
 
-- The program create a Sum cell at the end of each row
-- If a Column don't have an ID, we skip this column
-- If a the first cell of a Row is empty (no client name) -> we skip this row, EXECPT for the last row (it is a sum-row)
+- The program skips rows that don't have something in the first cell of a row. (AKA each row need to have a client)
--- spreadsheeter.py
+++ spreadsheeter.py
@@ -1,10 +1,12 @@
+from copy import Error
+from inspect import cleandoc
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import FORMULAE
 import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 
-EXECLS_PATHS = {"Table": "", "Prices": ""}
+EXECL_PATH = ""
 
 OUT_WORKBOOK_SUFFIX = "_out"
 DEBUG_LABEL = None
@@ -15,64 +17,102 @@ def update_debug_label(txt, color="red"):
 
 
 def get_out_filepath():
-    prefix, delimiter, postfix = EXECLS_PATHS["Table"].rpartition(".")
+    prefix, delimiter, postfix = EXECL_PATH.rpartition(".")
     return prefix + "_out" + delimiter + postfix
 
 
-def get_price_of_food_id(prices_sheet, id):
-    for col in prices_sheet.iter_cols(min_col=2, max_row=prices_sheet.max_row, max_col=prices_sheet.max_column):
-        if(col[0].value == id):
-            return col[-1].value
-
-    update_debug_label("There is anid in the table that is not in the prices!")
+def is_client_row(sheet, cell):
+    client_name = [r for r in sheet.iter_rows(
+        min_row=cell.row, max_row=cell.row)][0][0].value
+    return client_name != None
 
 
 def calculate_spreadsheet():
-    if(EXECLS_PATHS["Table"] == "" or EXECLS_PATHS["Prices"] == ""):
+    if(EXECL_PATH == ""):
         update_debug_label("You didn't select a table!")
         return
 
     table_workbook = load_workbook(
-        filename=EXECLS_PATHS["Table"], data_only=True)
-    # TODO: loop thourgh all the sheets
-    # for sheet in table_workbook.worksheets:
-    table_sheet = table_workbook.worksheets[0]
-
-    prices_workbook = load_workbook(filename=EXECLS_PATHS["Prices"])
-    prices_sheet = prices_workbook.active
-
-    # we save the max_column so when we create the sum cell the max_columns number don't grow
-    table_sheet_max_columns = table_sheet.max_column
-    for col in table_sheet.iter_cols(min_row=1, min_col=2, max_row=table_sheet.max_row, max_col=table_sheet_max_columns):
-        # if the id in the client worksheet is None or blank we skip the column
-        if(col[0].value == None or col[0].value == ""):
-            continue
-        col_food_price = get_price_of_food_id(prices_sheet, col[0].value)
-
-        # iterate through all the cell of current column and multiply by the price_id
-        # we start at the third row, because the first two are the id and the product name
-        # if this row don't have a client name we skip it except the last row, whice is a sum row of each column
-        for cell in col[2:]:
-            cellvalue_first_column_of_current_row = [r for r in table_sheet.iter_rows(
-                min_row=cell.row, max_row=cell.row)][0][0].value
-            if cellvalue_first_column_of_current_row == None and cell.row != table_sheet.max_row:
+        filename=EXECL_PATH, data_only=True)
+    # loop thorough each sheet and multiply each column by its price
+    for table_sheet in table_workbook.worksheets:
+        # we save the max_column so when we create the sum cell the max_columns number don't grow
+        table_sheet_max_columns = table_sheet.max_column
+        for col in table_sheet.iter_cols(min_row=1, min_col=1, max_row=table_sheet.max_row, max_col=table_sheet_max_columns):
+            # if the column don't have a price, skip the column
+            if not (type(col[0].value) == int or type(col[0].value) == float):
                 continue
+            col_price = col[0].value
 
-            cell.value = cell.value if cell.value else 0
-            cell.value = float(cell.value)*float(col_food_price)
-            # create or upated the sum cell of each row
-            sum_cell = table_sheet.cell(
-                row=cell.row, column=table_sheet_max_columns+1)
-            if sum_cell.value != None:
-                sum_cell.value = sum_cell.value + cell.value
+            # iterate through all the cell of current column and multiply by the price_id
+            # we start at the third row, because the first two are the id and the product name
+            # if this row don't have a client name we skip it except the last row, whice is a sum row of each column
+            for cell in col[2:]:
+                cellvalue_first_column_of_current_row = [r for r in table_sheet.iter_rows(
+                    min_row=cell.row, max_row=cell.row)][0][0].value
+                if not is_client_row(table_sheet, cell):
+                    continue
+
+                cell.value = cell.value if cell.value else 0
+                cell.value = 0 if cell.value == ' ' else cell.value
+                try:
+                    cell.value = float(cell.value)*float(col_price)
+                except:
+                    print(
+                        f"========\ncan't convert the cell[{cell.row},{cell.column_letter}]: '{cell.value}' to float. (sheet title: {table_sheet.title})\n=========")
+                    raise ValueError()
+                # create or upated the sum cell of each row
+                sum_cell = table_sheet.cell(
+                    row=cell.row, column=table_sheet_max_columns+1)
+                if sum_cell.value != None:
+                    sum_cell.value = sum_cell.value + cell.value
+                else:
+                    sum_cell.value = 0 + cell.value
+
+    # collect the sum data of each client for each sheet
+    clients_sum_data = {}
+    for table_sheet in table_workbook.worksheets:
+        for row in table_sheet.iter_rows(min_row=3, min_col=1, max_col=table_sheet.max_column, max_row=table_sheet.max_row):
+            if(not is_client_row(table_sheet, row[0])):
+                continue
+            if row[0].value in clients_sum_data:
+                clients_sum_data[row[0].value][table_sheet.title] = row[-1].value
             else:
-                sum_cell.value = 0 + cell.value
+                clients_sum_data[row[0].value] = {
+                    table_sheet.title: row[-1].value}
 
+    for client in clients_sum_data:
+        try:
+            clients_sum_data[client]["summery"] = sum(
+                clients_sum_data[client].values())
+        except:
+            print(f"{client}: {clients_sum_data[client]}")
+
+    # create a summery sheet and dump here the data
+    summery_sheet = table_workbook.create_sheet("summery")
+    summery_sheet.cell(row=1, column=1).value = "Clients"
+
+    dict_sheettitle_to_colnum = {}
+    col_counter = 2
+    for sheet in table_workbook.worksheets:
+        summery_sheet.cell(row=1, column=col_counter).value = sheet.title
+        dict_sheettitle_to_colnum[sheet.title] = col_counter
+        col_counter += 1
+
+    summery_row = 2
+    for client in sorted(clients_sum_data):
+        summery_sheet.cell(row=summery_row, column=1).value = client
+        for sheet in clients_sum_data[client]:
+            summery_sheet.cell(
+                row=summery_row, column=dict_sheettitle_to_colnum[sheet]).value = clients_sum_data[client][sheet]
+        summery_row += 1
+
+    # Save all sheets to new workbook
     table_workbook.save(filename=get_out_filepath())
     update_debug_label("Done!", "green")
 
 
-def create_frame(root, label_text, execl_path):
+def create_frame(root, label_text):
     load_frame = tk.Frame(master=root, relief=tk.GROOVE, borderwidth=5)
     tk.Label(master=load_frame,
              text=label_text).grid(row=1, column=1, padx=5, pady=5)
@@ -81,18 +121,18 @@ def create_frame(root, label_text, execl_path):
 
     # This is where we lauch the file manager bar.
     def OpenFile(label):
-        global EXECLS_PATHS
-        name = askopenfilename(initialdir="./",
-                               filetypes=(("Execl File", "*.xlsx"),
-                                          ("All Files", "*.*")),
-                               title="Choose a file."
-                               )
-        label_text = name
-        if(len(name) > 30):
-            label_text = "..." + name[-30:]
+        global EXECL_PATH
+        filename = askopenfilename(initialdir="./",
+                                   filetypes=(("Execl File", "*.xlsx"),
+                                              ("All Files", "*.*")),
+                                   title="Choose a file."
+                                   )
+        label_text = filename
+        if(len(filename) > 30):
+            label_text = "..." + filename[-30:]
 
         label.config(text=label_text)
-        EXECLS_PATHS[execl_path] = name
+        EXECL_PATH = filename
 
     tk.Button(master=load_frame, text="Browse",
               command=lambda: OpenFile(load_label)).grid(row=2, column=2, padx=5, pady=5)
@@ -112,11 +152,8 @@ def tk_window():
     DEBUG_LABEL.grid(row=4, column=1, padx=5, pady=5)
 
     table_frame = create_frame(
-        root, "Please select the execl file to SpreadSheet: ", "Table")
+        root, "Please select the execl file to SpreadSheet: ")
     table_frame.grid(row=1, column=1, padx=20, pady=10)
-    prices_frame = create_frame(
-        root, "Please select the prices execl file to use: ", "Prices")
-    prices_frame.grid(row=2, column=1, padx=20, pady=10)
 
     tk.Button(text="Finish",
               command=calculate_spreadsheet).grid(row=3, column=1, padx=20, pady=10)
